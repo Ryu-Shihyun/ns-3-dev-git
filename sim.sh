2bcd82cd3452b2b67f71d4bb232cafba7c072c59
Shihyun <willow.shihyun@keio.jp>
2022-11-23 14:36:22 +0900

    miner change

--- sim.sh
+++ sim.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
+ echo "Number of Station      MCS value     Channel width        GI       Throughput       Number of Basic        Number of Bsrp          Number of Conflict Sta"
 for i in `seq $1 $2`
 do
     if [ $((${i} % 10)) -eq 0 ];then
-       echo "Number of Station      MCS value     Channel width        GI       Throughput       Number of Basic        Number of Bsrp          Number of Conflict Sta"
         ./ns3 run "MBTA --mcs=8 --nStations=${i} --enableBsrp=true"
     fi
 done
