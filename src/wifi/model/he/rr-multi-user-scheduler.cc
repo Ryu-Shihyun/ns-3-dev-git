761606a3a345f9a118c4c7ac3db1cfc98d569809
Shihyun <willow.shihyun@keio.jp>
2022-11-24 15:29:19 +0900

    modify max number of accessible stations

--- src/wifi/model/he/rr-multi-user-scheduler.cc
+++ src/wifi/model/he/rr-multi-user-scheduler.cc
@@ -46,7 +46,7 @@ RrMultiUserScheduler::GetTypeId (void)
                    "The maximum number of stations that can be granted an RU in a DL MU OFDMA transmission",
                    UintegerValue (4),
                    MakeUintegerAccessor (&RrMultiUserScheduler::m_nStations),
-                   MakeUintegerChecker<uint8_t> (1, 74))
+                   MakeUintegerChecker<uint8_t> (1, 1000))
     .AddAttribute ("EnableTxopSharing",
                    "If enabled, allow A-MPDUs of different TIDs in a DL MU PPDU.",
                    BooleanValue (true),
