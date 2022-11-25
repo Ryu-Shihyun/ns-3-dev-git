4eb350b102ddc9256da5b9854f264fb108bcfc16
Shihyun <willow.shihyun@keio.jp>
2022-11-25 13:04:58 +0900

    test

--- sim.sh
+++ sim.sh
@@ -4,6 +4,6 @@
 for i in `seq $1 $2`
 do
     if [ $((${i} % 10)) -eq 0 ];then
-        ./ns3 run "MBTA --mcs=8 --nStations=${i} --enableBsrp=false"
+        ./ns3 run "MBTA --mcs=8 --nStations=${i} --enableBsrp=true"
     fi
 done
